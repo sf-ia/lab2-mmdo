b75d4d4e55edeaa5dddeaee6bf1b5fadf33ec3e1
gg <none>
2024-03-24 17:38:59 +0200

    Ініціалізація програми

---
+++ index.js
@@ -0,0 +1,12 @@
+const eq1 = new Equation([10, 5], 10);
+const eq2 = new Equation([6, 20], 10);
+const eq3 = new Equation([8, 15], 10);
+const g = new Equation([200, 300], 0, true);
+
+const st = new SimplexTable([eq1, eq2, eq3], g);
+
+function graphicallySolve() {
+    graphicalSolution([eq1, eq2, eq3], g);
+}
+
+window.st = st;
